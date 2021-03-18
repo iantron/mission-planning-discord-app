9dac085248e5286f8907cbfb586f973f2d2409fb
Ian Dahlke <ian.dahlke@gmail.com>
2021-03-17 22:39:05 -0600

    - set up app for python3 calls. may have to revert due to dev environment differences - made app instanceable - fixed tests to supply config to app - tests now sign payload same as discord - app config now comes from json file

--- app/api/app.py
+++ app/api/app.py
@@ -7,91 +7,97 @@ from .errors import errors
 from discord_interactions import verify_key_decorator, InteractionType, InteractionResponseType
 import airtable
 
-# Set up Discord
-client_public_key_path = os.getenv('CLIENT_PUBKEY_FILE')
-with open(client_public_key_path, "r") as f:
-    CLIENT_PUBLIC_KEY = f.read()
+# Set up Flask application factory
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.register_blueprint(errors)
 
-# Set up Airtable
-table_api_key_path = os.getenv('TABLE_API_KEY_FILE')
-with open(table_api_key_path, "r") as f:
-    AIRTABLE_API_KEY = f.read()
-
-# air_plan = airtable.Airtable(AIRTABLE_BASE_KEY, AIR_PLAN_)
-
-# Set up Flask application
-app = Flask(__name__)
-app.register_blueprint(errors)
-
-
-# Define Flask Routes
-@app.route("/")
-def index():
-    return Response("Hello, world!", status=200)
-
-
-@app.route("/custom", methods=["POST"])
-def custom():
-    payload = request.get_json()
-
-    if payload.get("say_hello") is True:
-        output = jsonify({"message": "Hello!"})
+    if test_config is None:
+         app.config.from_json(os.environ['APPLICATION_CONFIG_PATH'])
     else:
-        output = jsonify({"message": "..."})
-    return output
+        app.config.from_mapping(test_config)
+
+    CLIENT_PUBLIC_KEY = app.config['CLIENT_PUBLIC_KEY']
+
+    # Set up Airtable
+    #AIRTABLE_BASE_KEY = app.config['AIRTABLE_BASE_KEY']
+    #AIRTABLE_API_KEY = app.config['AIRTABLE_API_KEY']
+    #air_plan = airtable.Airtable(AIRTABLE_BASE_KEY, AIRTABLE_API_KEY)
+
+    try: os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Define Flask Routes
+    @app.route("/")
+    def index():
+        return Response("Hello, world!", status=200)
 
 
-@app.route("/health")
-def health():
-    return Response("OK", status=200)
+    @app.route("/custom", methods=["POST"])
+    def custom():
+        payload = request.get_json()
 
-#######################
-# Discord Interactions
-#######################
+        if payload.get("say_hello") is True:
+            output = jsonify({"message": "Hello!"})
+        else:
+            output = jsonify({"message": "..."})
+        return output
 
-@app.route('/interactions', methods=['POST'])
-@verify_key_decorator(CLIENT_PUBLIC_KEY)
-def interactions():
-  if request.json['type'] == InteractionType.APPLICATION_COMMAND:
-    data = request.json['data']
-    
-    if data['name'] == 'hello':
-        return jsonify({
-            'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            'data': {
-                'content': 'Hello world'
-            }
-        })
-    elif data['name'] == 'create-flight':
-        # air_plan.create_flight(data)
-        # callsign = data['options']['flight-callsign']
-        # task = data['options']['task']
-        # airframe = data['options']['airframe']
-        # datetime = data['options']['datetime']
-        # user_id = data['member']['user']['id']
-        # user_nick = data['member']['nick']
-        # user_roles = data['member']['roles']
-        # if FLIGHT_PLANNER not in user_roles:
-            # return jsonify({
-                # 'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                # 'data': {
-                    # 'content': 'You are not an authorized flight planner. Speak with your game admin.'
-                # }
-            # })
-        # else:
-            # air_plan.create_flight( data )
-            # return jsonify({
-                # 'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                # 'data': {
-                    # 'content': 'Flight registered'
-                # }
-            # })
-        return jsonify({
-            'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            'data': {
-                'content': 'Flight registered'
-            }
-        })
+
+    @app.route("/health")
+    def health():
+        return Response("OK", status=200)
+
+    #######################
+    # Discord Interactions
+    #######################
+
+    @app.route('/interactions', methods=['POST'])
+    @verify_key_decorator(CLIENT_PUBLIC_KEY)
+    def interactions():
+      if request.json['type'] == InteractionType.APPLICATION_COMMAND:
+        data = request.json['data']
+
+        if data['name'] == 'hello':
+            return jsonify({
+                'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                'data': {
+                    'content': 'Hello world'
+                }
+            })
+        elif data['name'] == 'create-flight':
+            # air_plan.create_flight(data)
+            # callsign = data['options']['flight-callsign']
+            # task = data['options']['task']
+            # airframe = data['options']['airframe']
+            # datetime = data['options']['datetime']
+            # user_id = data['member']['user']['id']
+            # user_nick = data['member']['nick']
+            # user_roles = data['member']['roles']
+            # if FLIGHT_PLANNER not in user_roles:
+                # return jsonify({
+                    # 'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    # 'data': {
+                        # 'content': 'You are not an authorized flight planner. Speak with your game admin.'
+                    # }
+                # })
+            # else:
+                # air_plan.create_flight( data )
+                # return jsonify({
+                    # 'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    # 'data': {
+                        # 'content': 'Flight registered'
+                    # }
+                # })
+            return jsonify({
+                'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                'data': {
+                    'content': 'Flight registered'
+                }
+            })
+
+    return app
         
         
         
--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,54 +1,55 @@
 import os
-import datetime
+import time
+import json
 import pytest
-from api.app import app
+from api.app import create_app
+from nacl.encoding import HexEncoder
 from nacl.signing import SigningKey
-
-client_public_key_path = './secrets/discord-public-key.secret'
-os.environ['CLIENT_PUBKEY_FILE'] = client_public_key_path
-table_api_key_path = './secrets/airtable-api-key.secret'
-os.environ['TABLE_API_KEY_FILE'] = table_api_key_path
+from flask import Flask, Response, jsonify, request
+from discord_interactions import verify_key_decorator, InteractionType, InteractionResponseType
 
 @pytest.fixture
-def client():
-    signing_key = SigningKey.generate()
-    verify_key = signing_key.verify_key
-    verify_key_bytes = verify_key.encode()
-    
-    with open(client_public_key_path, 'w') as f:
-        f.write(verify_key_bytes)
-    
-    with app.test_client() as client:
-        with client.session_transaction as sess:
-            sess['signing_key'] = signing_key
-            
-        yield client
+def signing_key():
+    signing_key = SigningKey(bytes.fromhex('543ec69d8f5afe234497f4aa3f656f5d71013bb14361e4b1c235a4e839715a34'))
+    return(signing_key)
 
+@pytest.fixture
+def verify_key(signing_key):
+    verify_key_hex = signing_key.verify_key.encode().hex()
+    return(str(verify_key_hex))
+
+@pytest.fixture
+def client(verify_key):
+    test_config = {}
+    test_config["CLIENT_PUBLIC_KEY"] = verify_key
+    flask_app = create_app(test_config = test_config)
+    flask_app.config['TESTING'] = True
+
+    with flask_app.test_client() as client:
+        yield client
 
 def test_index(client):
     assert client.get("/").status_code == 200
 
-
 def test_health(client):
     assert client.get("/health").status_code == 200
 
-def test_ping(client):
-    with client.session_transaction as sess:
-        signing_key = sess['signing_key']
-    
-    timestamp = datetime.datetime.now()
-    
-    data = jsonify({
-                    'type': InteractionType.PING
-                })
+def test_ping(signing_key, client):
+    timestamp = str(time.time())
+    print("Timestamp: ", timestamp)
 
-    message = timestamp.encode + data.encode
+    payload_dict = {'type': InteractionType.PING }
+    print("Data: ", payload_dict)
+
+    message = timestamp.encode() + json.dumps(payload_dict).encode()
+    print("Message", message)
     message_signed = signing_key.sign(message)
-    
+    print("Message Signature: ", message_signed.signature.hex())
+
     assert client.post("/interactions",
-        json=data,
+        json=payload_dict,
         headers={
-            'X-Signature-Ed25519' = message_signed.signature,
-            'X-Signature-Timestamp' = timestamp
+            'X-Signature-Ed25519': message_signed.signature.hex(),
+            'X-Signature-Timestamp': timestamp
         }
     ).status_code == 200
